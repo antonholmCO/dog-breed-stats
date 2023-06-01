0e219901640a65c8fbbe31053fa4352d2de949b9
Anton <anton.holm.y@gmail.com>
2023-06-01 12:37:37 +0200

    create footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <footer>
+        <div id="footer-text">
+            Copyright DogBreedPicker <span id="footer-year">2023</span>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+</style>
