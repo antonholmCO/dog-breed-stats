d9f0810004666bb0fe056249e5b75bc3f5dfe707
Anton <anton.holm.y@gmail.com>
2023-06-01 12:37:44 +0200

    add basic structure to views

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,12 @@
 </script>
 
 <template>
-    <h1>home</h1>
+    <h1>DogBreedPicker</h1>
+    <h2>Information about your dog breed</h2>
+    <form>
+        <input id="search-field" type="search" />
+        <button id="search-button">Search</button>
+    </form>
 </template>
 
 <style>
--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -2,7 +2,29 @@
 </script>
 
 <template>
-    <h1>stats page</h1>
+    <div id="result-card">
+        <img id="result-img" src="https://api-ninjas.com/images/dogs/french_bulldog.jpg" alt="" />
+        <div id="result-text">
+            <h1>[dog breed]</h1>
+            <p>[general description]</p>
+        </div>
+    </div>
+    
+    <section id="statistics-section">
+        <h2><span>[breed]</span> statistics</h2>
+        
+        <div id="statistics">
+            <div class="stat">
+                <span class="stat-title">Barking</span>
+                <div class="stat-value">1/5</div>
+            </div>
+            <div class="stat">
+                <span class="stat-title">Energy</span>
+                <div class="stat-value">3/5</div>
+            </div>
+        </div>
+        
+    </section>
 </template>
 
 <style>
