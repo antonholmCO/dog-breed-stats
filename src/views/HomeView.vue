8ae9d491738c7bfd756c7055bde29c601e92652d
Anton <anton.holm.y@gmail.com>
2023-06-01 13:01:54 +0200

    implement basic functionallity to make a GET request to dog api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,35 @@
 <script setup>
+import { ref } from "vue";
+import axios from "axios";
+import apikey from "../apikey";
+
+const userInput = ref("");
+
+function getDogBreed(e) {
+    e.preventDefault();
+    queryApi(userInput.value);
+}
+
+function queryApi(dogBreed) {
+    axios({
+        method: "GET",
+        url: "https://api.api-ninjas.com/v1/dogs?name=" + dogBreed,
+        headers: {"X-Api-Key": apikey},
+        contentType: "application/json"
+    })
+    .then(function (response) {
+        console.log(response.data);
+    });
+}
+
 </script>
 
 <template>
     <h1>DogBreedPicker</h1>
     <h2>Information about your dog breed</h2>
     <form>
-        <input id="search-field" type="search" />
-        <button id="search-button">Search</button>
+        <input v-model="userInput" id="search-field" />
+        <button id="search-button" @click="getDogBreed">Search</button>
     </form>
 </template>
 
