3f9c88db36d3cfab6d366d06f658a8383ab5c05c
Anton <anton.holm.y@gmail.com>
2023-06-01 22:07:32 +0200

    create stat component

---
+++ src/components/Stat.vue
@@ -0,0 +1,60 @@
+<script setup>
+    import { ref, onMounted } from 'vue'
+    import Chart from 'chart.js/auto'
+
+    const statChart = ref(null)
+
+    const props = defineProps(["attribute", "value", "positive"]);
+
+    let value = props.value;
+
+    let gray = "#e5e7eb";
+    let red = "#ef4444";
+    let yellow = "#fcd34d";
+    let softGreen = "#bef264";
+    let mediumGreen = "#a3e635"
+    let fullGreen = "#22c55e";
+
+    let colors = [
+        red,
+        yellow,
+        softGreen,
+        mediumGreen,
+        fullGreen
+    ];
+
+    for (let i = (colors.length); i >= value; i--) {
+        colors[i] = gray;
+    }
+
+    onMounted(() => {
+      new Chart(statChart.value, {
+        type: 'doughnut',
+        data: {
+            datasets: [
+                {
+                    data: [1, 1, 1, 1, 1]
+                }
+            ]
+        },
+        options: {
+            backgroundColor: colors,
+            cutout: 100,
+            spacing: 3
+        },
+      })
+    })
+
+
+
+</script>
+
+<template>
+    <div class="stat flex flex-col max-w-xs">
+        <span class="stat-title mx-auto text-lg">{{ props.attribute }}</span>
+        <canvas ref="statChart" class="w-2"></canvas>
+    </div>
+</template>
+
+<style>
+</style>
