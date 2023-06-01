6fafd90401ca33c2cef29276622282c886e15588
Anton <anton.holm.y@gmail.com>
2023-06-01 19:12:07 +0200

    add functionality to add loading bar before all data has loaded

--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -8,6 +8,7 @@
     const router = useRouter();
     const route = useRoute();
     
+    let loadingPage = ref(true);
     let dogInfo = ref({});
     let dogDataStr = history.state.dogDataStr;
     let dogBreed = route.params.breedName;
@@ -25,6 +26,7 @@
             }
 
             dogInfo.value = response.data[0];
+            loadingPage.value = false;
         })
         .catch(function(error) {
             console.log("erorrrrr")
@@ -34,6 +36,7 @@
     } else {
         let dogData = JSON.parse(dogDataStr);
         dogInfo.value = dogData;
+        loadingPage.value = false;
     }
 
 
@@ -42,29 +45,35 @@
 </script>
 
 <template>
-    <div id="result-card">
-        <img id="result-img" src="https://api-ninjas.com/images/dogs/french_bulldog.jpg" alt="" />
-        <div id="result-text">
-            <h1>{{ dogBreed }}</h1>
-            <p>[general description]</p>
-        </div>
+    <div v-if="loadingPage">
+        <h1>LOADING</h1>
     </div>
-    
-    <section id="statistics-section">
-        <h2><span>{{ dogBreed }}</span> Statistics</h2>
-        
-        <div id="statistics">
-            <div class="stat">
-                <span class="stat-title">Barking</span>
-                <div class="stat-value">{{ dogInfo.barking }}</div>
-            </div>
-            <div class="stat">
-                <span class="stat-title">Energy</span>
-                <div class="stat-value">1/5</div>
+
+    <div v-else>
+        <div id="result-card">
+            <img id="result-img" src="https://api-ninjas.com/images/dogs/french_bulldog.jpg" alt="" />
+            <div id="result-text">
+                <h1>{{ dogBreed }}</h1>
+                <p>[general description]</p>
             </div>
         </div>
-    </section>
-    <RouterLink to="/">Home</RouterLink>
+        
+        <section id="statistics-section">
+            <h2><span>{{ dogBreed }}</span> Statistics</h2>
+            
+            <div id="statistics">
+                <div class="stat">
+                    <span class="stat-title">Barking</span>
+                    <div class="stat-value">{{ dogInfo.barking }}</div>
+                </div>
+                <div class="stat">
+                    <span class="stat-title">Energy</span>
+                    <div class="stat-value">1/5</div>
+                </div>
+            </div>
+        </section>
+        <RouterLink to="/">Home</RouterLink>
+    </div>
 </template>
 
 <style>
